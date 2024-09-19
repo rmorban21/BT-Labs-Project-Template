057bd05033b9a5f78507a2f3fcecd6866dc2802c
Rich Morban <richardmorban21@gmail.com>
2024-09-18 20:41:26 -0400

    N/A

--- src/main/java/com/hcc/entities/User.java
+++ src/main/java/com/hcc/entities/User.java
@@ -1,6 +1,5 @@
 package com.hcc.entities;
 
-import javax.annotation.processing.Generated;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
